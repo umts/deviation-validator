039effd9ad6beb0b83244c8ffe5fce1b2d7e1e23
David Faulkenberry <david.faulkenberry@gmail.com>
2017-07-18 15:42:12 -0400

    Deviation validator

---
+++ deviation_validator.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require 'json'
+require 'net/http'
+require 'pry-byebug'
+
+module DeviationValidator
+  raise <<~MESSAGE unless File.file? 'stops.txt'
+    No config file found. Please see the stops.txt.example \
+    file and create a stops.txt file to match.
+  MESSAGE
+
+  STOP_NAMES = File.read('stops.txt').lines.map(&:strip)
+
+  STOP_IDS = JSON.parse File.read('stops.json')
+
+  PVTA_API_URL = 'http://bustracker.pvta.com/InfoPoint/rest'
+
+  STOP_NAMES.each do |name|
+    stop_id = STOP_IDS[name]
+    departures_uri = URI([PVTA_API_URL, 'stopdepartures', 'get', stop_id].join('/'))
+    departures = JSON.parse(Net::HTTP.get(departures_uri))
+    route_directions = departures.first.fetch 'RouteDirections'
+    route_directions.each do |route_dir|
+      departures = route_dir.fetch 'Departures'
+      departures.each do |dept|
+        # Example: "00:05:30", meaning that the bus is 5 1/2 minutes behind
+        deviation = dept.fetch 'Dev'
+        if deviation[0] == '-'
+          # THE BUS IS EARLY!
+        else
+          hours, minutes, _seconds = deviation.split(':').map(&:to_i)
+          if hours > 0 || minutes > 10
+            # THE BUS IS LATE!
+          end
+        end
+      end
+    end
+  end
+end
