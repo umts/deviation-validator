719420a6f05a5bda8fdb1b023f654fac4235fb23
David Faulkenberry <david.faulkenberry@gmail.com>
2017-07-21 09:28:10 -0400

    Restructure into module methods and a Rakefile

---
+++ Rakefile
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative 'deviation_validator'
+include DeviationValidator
+
+namespace :deviation_validator do
+  task :search do
+    DeviationValidator.search
+  end
+end
--- deviation_validator.rb
+++ deviation_validator.rb
@@ -18,28 +18,39 @@ module DeviationValidator
 
   DAILY_LOG_FILE = "log/#{Time.now.strftime('%Y-%m-%d')}.txt"
 
-  STOP_NAMES.each do |name|
-    stop_id = STOP_IDS[name]
+  def query_departures(stop_id)
     departures_uri = URI([PVTA_API_URL, 'stopdepartures', 'get', stop_id].join('/'))
-    departures = JSON.parse(Net::HTTP.get(departures_uri))
-    route_directions = departures.first.fetch 'RouteDirections'
-    route_directions.each do |route_dir|
-      departures = route_dir.fetch 'Departures'
-      departures.each do |dept|
-        # Example: "00:05:30", meaning that the bus is 5 1/2 minutes behind
-        deviation = dept.fetch 'Dev'
-        if deviation[0] == '-'
-          # THE BUS IS EARLY!
-        else
-          hours, minutes, _seconds = deviation.split(':').map(&:to_i)
-          if hours > 0 || minutes > 10
-            # THE BUS IS LATE!
-            trip = dept.fetch('Trip')
-            run_id = trip.fetch('RunId')
-            headsign = trip.fetch('InternetServiceDesc')
-            timestamp = Time.now.strftime '%l:%M %P'
-            File.open DAILY_LOG_FILE, 'a' do |file|
-              file.puts "#{timestamp}, #{name}: Run #{run_id} (#{headsign}), deviation #{deviation}"
+    JSON.parse(Net::HTTP.get(departures_uri))
+  end
+
+  def report_deviation(departure)
+    trip = departure.fetch('Trip')
+    run_id = trip.fetch('RunId')
+    headsign = trip.fetch('InternetServiceDesc')
+    timestamp = Time.now.strftime '%l:%M %P'
+    File.open DAILY_LOG_FILE, 'a' do |file|
+      file.puts "#{timestamp}, #{name}: Run #{run_id} (#{headsign}), deviation #{deviation}"
+    end
+  end
+
+  def search
+    STOP_NAMES.each do |name|
+      stop_id = STOP_IDS[name]
+      departures = query_departures(stop_id)
+      route_directions = departures.first.fetch 'RouteDirections'
+      route_directions.each do |route_dir|
+        departures = route_dir.fetch 'Departures'
+        departures.each do |departure|
+          # Example: "00:05:30", meaning that the bus is 5 1/2 minutes behind
+          deviation = departure.fetch 'Dev'
+          if deviation[0] == '-'
+            # THE BUS IS EARLY!
+            report_deviation(departure)
+          else
+            hours, minutes, _seconds = deviation.split(':').map(&:to_i)
+            if hours > 0 || minutes > 10
+              # THE BUS IS LATE!
+              report_deviation(departure)
             end
           end
         end
